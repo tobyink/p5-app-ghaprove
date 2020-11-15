85a88780cd042005bdf7ac007e7b3e1d7df3bc3a
Toby Inkster <mail@tobyinkster.co.uk>
2020-11-15 19:51:20 +0000

    make the module do something useful

--- lib/App/GhaProve.pm
+++ lib/App/GhaProve.pm
@@ -7,6 +7,48 @@ package App::GhaProve;
 our $AUTHORITY = 'cpan:TOBYINK';
 our $VERSION   = '0.004';
 
+sub _system { scalar system( @_ ) }
+
+sub go {
+	my ( @args ) = @_;
+	my $system = ref( $args[0] ) eq 'CODE' ? shift(@args) : \&_system;
+	
+	my $testing_mode = $ENV{GHA_TESTING_MODE}  || 0;
+	my $coverage     = $ENV{GHA_TESTING_COVER} || 0;
+	
+	if ( $coverage =~ /^(true|1)$/i ) {
+		if ( length $ENV{HARNESS_PERL_SWITCHES} ) {
+			$ENV{HARNESS_PERL_SWITCHES} .= ' ';
+		}
+		else {
+			$ENV{HARNESS_PERL_SWITCHES} = '';
+		}
+		$ENV{HARNESS_PERL_SWITCHES} .= '-MDevel::Cover';
+	}
+	
+	my @errors;
+	
+	if ( $testing_mode !~ /^(extended|1)$/i ) {
+		delete $ENV{EXTENDED_TESTING};
+		print "# ~~ Standard testing\n";
+		push @errors, $system->( 'prove', @args );
+	}
+
+	if ( $testing_mode =~ /^(extended|both|1|2)$/i ) {
+		$ENV{EXTENDED_TESTING} = 1;
+		print "# ~~ Extended testing\n";
+		push @errors, $system->( 'prove', @args );
+	}
+	
+	my ( $max ) = sort { $b <=> $a } @errors;
+	
+	if ( $max > 254 ) {
+		$max = 254;
+	}
+	
+	return $max;
+}
+
 1;
 
 __END__
@@ -19,23 +61,56 @@ __END__
 
 App::GhaProve - provides gha-prove app
 
+=head1 SYNOPSIS
+
+From command-line:
+
+  $ gha-prove -b -r -v 't'
+  ...
+
+In script:
+
+  use App::GhaProve;
+  my @args = qw( -b -r -v t );
+  my $exit = 'App::GhaProve'->go( @args );
+  exit( $exit );
+
+With a callback (instead of C<< CORE::system >>):
+
+  use App::GhaProve;
+  my @args = qw( -b -r -v t );
+  my $exit = 'App::GhaProve'->go( sub { ... }, @args );
+  exit( $exit );
+
+=head1 DESCRIPTION
+
+C<< gha-prove >> is just a small wrapper around the C<< prove >> command.
+It will inspect C<< GHA_* >>> environment variables and this will affect
+how it calls C<< prove >>, perhaps calling C<< prove >> multiple times.
+It is intended to be used in continuous integration environments, such as
+GitHub Actions.
+
 =head1 ENVIRONMENT
 
-C<< GHA_TESTING_COVER=1 >> or C<< GHA_TESTING_COVER=true >>
+=over
+
+=item C<< GHA_TESTING_COVER=1 >> or C<< GHA_TESTING_COVER=true >>
 
 Turn on Devel::Cover.
 
-C<< GHA_TESTING_MODE=0 >> or C<< GHA_TESTING_MODE=standard >> 
+=item C<< GHA_TESTING_MODE=0 >> or C<< GHA_TESTING_MODE=standard >> 
 
 Run test suite without EXTENDED_TESTING.
 
-C<< GHA_TESTING_MODE=1 >> or C<< GHA_TESTING_MODE=extended >> 
+=item C<< GHA_TESTING_MODE=1 >> or C<< GHA_TESTING_MODE=extended >> 
 
 Run test suite with EXTENDED_TESTING=1.
 
-C<< GHA_TESTING_MODE=2 >> or C<< GHA_TESTING_MODE=both >> 
+=item C<< GHA_TESTING_MODE=2 >> or C<< GHA_TESTING_MODE=both >> 
 
 Run test suite twice, using each of the above.
+
+=back
 
 =head1 BUGS
 
